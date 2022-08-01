92a1e63373c7e8225673081e01f829e9fa2a98f2
sleepy0owl <souravcristiano502@gmail.com>
2022-08-01 13:13:57 +0530

    Add user data as shell script

---
+++ lib/post_initialization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update -y
+
+apt-get install git -y
+
+apt-get install unzip -y
+
+
+git clone https://github.com/aws-samples/unishop-monolith-to-microservices.git /home/ubuntu/MonoToMicro
+
+
